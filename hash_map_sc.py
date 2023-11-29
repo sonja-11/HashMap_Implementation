d8362e9e9efccd23df1c2ba49d727e34ee9ad88b
sonja-11 <hanemans@oregonstate.edu>
2023-11-29 14:12:43 -0800

    created code

--- hash_map_sc.py
+++ hash_map_sc.py
@@ -97,6 +97,11 @@ class HashMap:
         capacity when this method is called and the current load factor of the table is
         greater than or equal to 1.0.
         """
+
+        load_factor = (self._size + 1) / self._capacity
+        if load_factor >= 1.0:
+            self.resize_table(self._capacity * 2)
+
         hash = self._hash_function(key)
         index = hash % self._capacity
         bucket = self._buckets[index]
@@ -107,10 +112,6 @@ class HashMap:
 
         bucket.insert(key, value)
         self._size += 1
-
-        load_factor = (self._size + 1) / self._capacity
-        if load_factor >= 1.0:
-            self.resize_table(self._capacity * 2)
 
     def resize_table(self, new_capacity: int) -> None:
         """
@@ -132,7 +133,7 @@ class HashMap:
         for i in range(new_capacity):
             new_table.append(LinkedList())
 
-        for i in range(self._buckets.length()):
+        for i in range(self._capacity):
             bucket = self._buckets[i]
             for j in bucket:
                 index = self._hash_function(j.key) % new_capacity
@@ -152,7 +153,7 @@ class HashMap:
         Returns the number of empty buckets in the hash table.
         """
         count = 0
-        for i in range(self._buckets.length()):
+        for i in range(self._capacity):
             if self._buckets[i].length() == 0:
                 count += 1
         return count
@@ -183,6 +184,8 @@ class HashMap:
         for i in bucket:
             if i.key == key:
                 return True
+            else:
+                return False
         return False
 
     def remove(self, key: str) -> None:
